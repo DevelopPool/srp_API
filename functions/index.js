bb781374bbcc43d7da935fc14920cab47edc72a0
phillips <whyang9701@gmail.com>
2019-02-23 17:02:12 +0800

    修正register 驗證相關欄位 完成 loginCheck util.js 新增資料表定義

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,8 @@ admin.initializeApp();
 const firestore = admin.firestore();
 const auth = admin.auth();
 
+
+
 exports.register = functions.https.onRequest((request, response) => {
 
     let resultObj = {
@@ -16,7 +18,7 @@ exports.register = functions.https.onRequest((request, response) => {
     };
 
     //normalize
-    let defaultValue = "";
+    let defaultValue = " ";
     let _name = util.checkEmpty(request.body.name) ? request.body.name : defaultValue;
     let _phoneNumber = util.checkEmpty(request.body.phoneNumber) ? request.body.phoneNumber : defaultValue;
     let _gender = util.checkEmpty(request.body.gender) ? request.body.gender : defaultValue;
@@ -28,43 +30,67 @@ exports.register = functions.https.onRequest((request, response) => {
     let _image = defaultValue;
 
 
+    //確認firestore account 是否存在
+    let firestoreAccount = firestore.collection(util.tables.users.tableName).where(util.tables.users.columns.phoneNumber, '==', _phoneNumber)
+        .limit(1).get().then(snapshot => {
+            if (snapshot.size === 0) {
+                return false
+            }
+            else {
+                return true;
+            }
+
+        });
+
+
     //確認gender存在
+    let genderCheck = firestore.collection(util.tables.gender.tableName).doc(_gender).get().then(snapshot => {
+        if (snapshot.exists) {
+            return Promise.resolve('gender exists');
+        }
+        else {
+            return Promise.reject('gender does not exists');
+        }
+    });
 
     //確認jobTitle存在？
 
     //確認team存在
-    // if (_team === "") {
-    //     console.log('team is empty');
-    //     response.json(resultObj);
-    //     return;
-    // }
-    // else {
-    //     firestore.collection('team').doc(_team).get().then(doc => {
-    //         if (doc.exists) {
-
-    //         }
-    //         else {
-    //             console.log(request.body);
-    //             console.log('team does not exist');
-    //             response.json(resultObj);
-    //             return;
-    //         }
-    //     });
-    // }
-
-
-    //response.json(resultObj);
+    let teamCheck = firestore.collection(util.tables.team.tableName).doc(_team).get().then(snapshot => {
+        if (snapshot.exists) {
+            return Promise.resolve('teamCheck exists');
+        }
+        else {
+            return Promise.reject('teamCheck does not exists');
+        }
+    });
 
     //確認workingType存在
+    let workingTypeCheck = firestore.collection(util.tables.workingType.tableName).doc(_workingType).get().then(snapshot => {
+        if (snapshot.exists) {
+            return Promise.resolve('workingType exists');
+        }
+        else {
+            return Promise.reject('workingType does not exists');
+        }
+    });
 
-    //確認phoneNumber 不為空
-    //確認phoneNumber 不重複
-    admin.auth().createUser({
-        phoneNumber: _phoneNumber,
-    }).then(userRecord => {
-        console.log(userRecord);
-        admin.firestore().collection('users').doc(userRecord.uid).set(
-            {
+    //確認auth account 是否存在
+    let accountExists = auth.getUserByPhoneNumber(_phoneNumber).then(userRecord => {
+        return userRecord;
+    }).catch(reason => {
+        return false;
+    });
+
+
+    Promise.all([genderCheck, teamCheck, workingTypeCheck, accountExists, firestoreAccount]).then(value => {
+        //firestore 帳號已經存在
+        if (value[4] === true) {
+            return Promise.reject({ log: "帳號已經存在" });
+        }
+        //如果auth帳號已經存在
+        else if (value[3] !== false) {
+            return firestore.collection(util.tables.users.tableName).doc(value[3].uid).set({
                 name: _name,
                 phoneNumber: _phoneNumber,
                 gender: _gender,
@@ -74,23 +100,123 @@ exports.register = functions.https.onRequest((request, response) => {
                 verified: _verified,
                 permission: _permission,
                 image: _image,
-            })
-    }).then(documentReference => {
+            });
+        }
+        //都不存在
+        else {
+            return auth.createUser({
+                phoneNumber: _phoneNumber,
+            }).then(userRecord => {
+                return firestore.collection(util.tables.users.tableName).doc(value[3].uid).set({
+                    name: _name,
+                    phoneNumber: _phoneNumber,
+                    gender: _gender,
+                    jobTitle: _jobTitle,
+                    team: _team,
+                    workingType: _workingType,
+                    verified: _verified,
+                    permission: _permission,
+                    image: _image,
+                });
+            });
+        }
+
+        
+    }).then(() => {
         resultObj.excutionResult = 'success';
         response.json(resultObj);
     }).catch(reason => {
         console.log(reason)
         response.json(resultObj);
+    })
+
+    // admin.auth().createUser({
+    //     phoneNumber: _phoneNumber,
+    // }).then(userRecord => {
+    //     console.log(userRecord);
+    //     admin.firestore().collection('users').doc(userRecord.uid).set(
+    //         {
+    //             name: _name,
+    //             phoneNumber: _phoneNumber,
+    //             gender: _gender,
+    //             jobTitle: _jobTitle,
+    //             team: _team,
+    //             workingType: _workingType,
+    //             verified: _verified,
+    //             permission: _permission,
+    //             image: _image,
+    //         })
+    // }).then(documentReference => {
+    //     resultObj.excutionResult = 'success';
+    //     response.json(resultObj);
+    // }).catch(reason => {
+    //     console.log(reason)
+    //     response.json(resultObj);
+    // });
+
+});
+
+
+exports.checkLogin = functions.https.onRequest((request, response) => {
+    let resultObj = {
+        excutionResult: 'fail',
+    };
+
+    let defaultValue = "";
+    let uid = util.checkEmpty(request.body.uid) ? request.body.uid : defaultValue;
+
+    auth.getUser(uid).then(userRecord => {
+
+        // 確認 lastSignInTime存在
+        if (userRecord.metadata.lastSignInTime === null) {
+            return Promise.reject({ uid: userRecord.uid, log: "last Log In Time Null" });
+        }
+        else {
+            //確認 時間合理 十分鐘以內
+            let differSecond = (Date.now() - new Date(userRecord.metadata.lastSignInTime)) / 1000;
+            console.log(differSecond);
+            if (differSecond > 600) {
+                return Promise.reject({ uid: userRecord.uid, log: "last Log In Time over 600 seconds" });
+            }
+
+            // 寫入 login record 
+            return firestore.collection(util.tables.loginRecord.tableName).add({
+                uid: userRecord.uid,
+                loginTime: Date.now(),
+            })
+           
+        }
+    }).then((loginTime) => {
+        // 回傳成功
+        resultObj.excutionResult = 'success';
+        response.json(resultObj);
+    }).catch(reason => {
+        console.log(reason);
+        response.json(resultObj);
     });
 
 });
 
-// exports.checkLogin = functions.https.onRequest((request, response) => {
-
-// });
-
 // exports.logout = functions.https.onRequest((request, response) => {
+//     let resultObj = {
+//         excutionResult: 'fail',
+//     };
 
+//     let defaultValue = "";
+//     let uid = util.checkEmpty(request.body.uid) ? request.body.uid : defaultValue;
+
+//     auth.getUser(uid).then(userRecord => {
+//         //確認 loginRecord 存在
+//         return firestore.collection(util.tables.loginRecord).where(util.tables.loginRecord.colunms.uid,'==',userRecord.uid).orderBy(util.tables.loginRecord.colunms.loginTime,"desc").limit(1).get();
+//     }).then((snapshot=>{
+//         console.log(snapshot);
+//     })).then((loginTime) => {
+//         resultObj.excutionResult = 'success';
+//         response.json(resultObj);
+//     }).catch(reason => {
+//         console.log(reason);
+//         response.json(resultObj);
+//     });
 // });
 
 // exports.updateUser = functions.https.onRequest((request, response) => {
--- functions/util.js
+++ functions/util.js
@@ -1,8 +1,42 @@
 
 //驗證資料是否為空
-exports.checkEmpty=function checkEmpty(uncheckedValue) {
+exports.checkEmpty = function checkEmpty(uncheckedValue) {
     if (uncheckedValue) {
         return true;
     }
     return false;
+}
+
+exports.tables = {
+    users: {
+        tableName: 'users',
+        columns: {
+            gender:'gender',
+            phoneNumber:'phoneNumber'
+        }
+    },
+    gender: {
+        tableName: 'gender',
+        columns: {
+            showName: 'showName',
+        }
+    },
+    team: {
+        tableName: 'team',
+    },
+    workingType: {
+        tableName: 'workingType',
+        columns: {
+            showName: 'showName',
+        }
+    },
+    loginRecord: {
+        tableName: 'loginRecord',
+        columns: {
+            uid: 'uid',
+            loginTime: 'loginTime',
+            logoutMethod: 'logoutMethod',
+            logoutTime: 'logoutTime'
+        }
+    },
 }
